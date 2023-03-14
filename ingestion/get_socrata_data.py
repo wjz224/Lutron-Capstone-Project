99c33655e26916568452f0e3a8aaddd46e3a1b1c
Ayon Bhowmick <ayb224@lehigh.edu>
2023-03-14 15:06:06 -0400

    comments

--- ingestion/get_socrata_data.py
+++ ingestion/get_socrata_data.py
@@ -1,24 +1,35 @@
+"""This script is used to download data from cities  which use Socrata APIs"""
+
 from sodapy import Socrata
 import pandas as pd
 
-cities: "dict[str, str]" = {"data.cityofnewyork.us":"dm9a-ab7w",
+# dictionary of the city api urls and their dataset ids
+CITIES: "dict[str, str]" = {"data.cityofnewyork.us":"dm9a-ab7w",
         "data.lacity.org":"nbyu-2ha9",
         "data.austintexas.gov": "3syk-w9eu",
         "data.cityofchicago.org": "ydr8-5enu",
         "data.mesaaz.gov": "2gkz-7z4f"}
 
-def get_data(url, dataset_id):
+def get_data(url, dataset_id) -> None:
+    """
+    Pulls data from a Socrata API and saves it as a csv file
+    :param url: the url of the api
+    :param dataset_id: the id of the dataset
+    :return: None
+    """
     client = Socrata(url, None)
     results = client.get(dataset_id)
     df = pd.DataFrame.from_dict(results)
+    # Uses part of the url as the name of the csv file
     df.to_csv(f"raw_data/{url.split('.')[1]}.csv")
     print(f"Saved {url.split('.')[1]}.csv")
 
 def main():
-    for city in cities:
+    for city in CITIES:
+        # The API sometimes times out, so this loop will try to get the data until it succeeds
         while 1:
             try:
-                get_data(city, cities[city])
+                get_data(city, CITIES[city])
                 break
             except:
                 pass
