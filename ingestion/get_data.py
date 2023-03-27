85fc6047605c68111ae2de015554680589665e17
Ayon Bhowmick <ayonbhowmick2002@gmail.com>
2023-03-27 17:09:14 -0400

    combine getting data files and remove csv

--- ingestion/get_data.py
+++ ingestion/get_data.py
@@ -1,15 +1,16 @@
 import logging
 import pandas as pd
+from sodapy import Socrata
 
-#import ssl
-#ssl._create_default_https_context = ssl._create_unverified_context
+# Dictionary of cities and their corresponding API information
+META_CITY = {"new_york":("socrata", "data.cityofnewyork.us", "dm9a-ab7w"),
+            "chicago": ("socrata", "data.cityofchicago.org", "ydr8-5enu"),
+            "mesa": ("socrata", "data.mesaaz.gov", "2gkz-7z4f"),
+            "la": ("socrata", "data.lacity.org", "nbyu-2ha9"),
+            "austin": ("socrata", "data.austintexas.gov", "3syk-w9eu"),
+            "philly": ("non_socrata", "https://phl.carto.com/api/v2/sql?filename=permits&format=csv&skipfields=cartodb_id,the_geom,the_geom_webmercator&q=SELECT%20*,%20ST_Y(the_geom)%20AS%20lat,%20ST_X(the_geom)%20AS%20lng%20FROM%20permits%20WHERE%20permitissuedate%20%3E=%20%272016-01-01%27")}
 
-
-# Define the URL of the database
-# dictionary of the city api urls and their dataset ids
-CITIES: "dict[str]" = {'https://phl.carto.com/api/v2/sql?filename=permits&format=csv&skipfields=cartodb_id,the_geom,the_geom_webmercator&q=SELECT%20*,%20ST_Y(the_geom)%20AS%20lat,%20ST_X(the_geom)%20AS%20lng%20FROM%20permits%20WHERE%20permitissuedate%20%3E=%20%272016-01-01%27'}
-
-def get_data(url) -> None:
+def get_data(url, location):
     """
     Pulls data from a Non-Socrata source and saves it as a csv file
     :param url: the url of the api
@@ -18,19 +19,45 @@ def get_data(url) -> None:
     # Use Pandas to read the CSV file from the URL
     df = pd.read_csv(url)
     # Save the CSV file to the current directory
-    df.to_csv(f"raw_data/{url.split('.')[0]}.csv", index=False)
-    print(f"Saved {url.split('.')[1]}.csv")
+    df.to_csv(f"ingestion/raw_data/{location}.csv", index=False)
+    logging.info(f"Saved {location}.csv")
 
+def get_socrata_data(url, dataset_id, location):
+    """
+    Pulls data from a Socrata API and saves it as a csv file
+    :param url: the url of the api
+    :param dataset_id: the id of the dataset
+    """
+    client = Socrata(url, None)
+    results = client.get(dataset_id)
+    df = pd.DataFrame.from_dict(results)
+    # Uses part of the url as the name of the csv file
+    df.to_csv(f"ingestion/raw_data/{location}.csv")
+    logging.info(f"Saved {location}.csv")
 
 def main():
-    for city in CITIES:
+    locations = ["philly"]
+    for place in locations:
+        meta = META_CITY[place]
+        if meta[0] == "socrata":
+            fail_count = 0
+            while 1:
+                try:
+                    get_socrata_data(meta[1], meta[2], place)
+                    break
+                except Exception as e:
+                    logging.error(e)
+                    fail_count += 1
+                    if fail_count == 3:
+                        logging.error(f"Failed to get data from {place}")
+                        break
+                    pass
+        elif meta[0] == "non_socrata":
             try:
-                get_data(city)
-                break
+                get_data(meta[1], place)
             except Exception as e:
                 logging.error(e)
-                pass
+
 
 if __name__ == "__main__":
     main()
-
