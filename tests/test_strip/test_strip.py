e2855cf14d5a4e1f1b0a2654f45a9dfbff7f7d5f
Ayon Bhowmick <ayonbhowmick2002@gmail.com>
2023-03-31 08:59:54 -0400

    comments

--- tests/test_strip/test_strip.py
+++ tests/test_strip/test_strip.py
@@ -1,3 +1,5 @@
+"""These are the unit tests for the strip_data.py scripts."""
+
 import pandas as pd
 import unittest
 import os
@@ -8,6 +10,9 @@ from strip_data import strip_dataframes
 
 class GetDataTest(unittest.TestCase):
     def test_austin(self, location="austin"):
+        """
+        Tests the stripping function for Austin and compares the output to the true output. This function is reused for the tests for the other cities
+        """
         log = logging.getLogger("test")
         log.debug(f"Testing {location}")
         data = strip_dataframes([location])
@@ -19,15 +24,27 @@ class GetDataTest(unittest.TestCase):
         os.remove(f"./stripped_data/{location}.csv")
 
     def test_new_york(self):
+        """
+        Tests the stripping function for New York.
+        """
         self.test_austin("new_york")
 
     def test_chicago(self):
+        """
+        Tests the stripping function for Chicago.
+        """
         self.test_austin("chicago")
 
     def test_mesa(self):
+        """
+        Tests the stripping function for Mesa.
+        """
         self.test_austin("mesa")
 
     def test_la(self):
+        """
+        Tests the stripping function for Los Angeles.
+        """
         self.test_austin("la")
 
 if __name__ == "__main__":
