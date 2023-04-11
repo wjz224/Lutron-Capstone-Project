dd57b50f03ee4b4b80265f836a33449566b2e116
Ayon Bhowmick <ayb224@lehigh.edu>
2023-04-11 01:05:20 -0400

    moved paths to variables

--- ingestion/strip_data.py
+++ ingestion/strip_data.py
@@ -9,7 +9,9 @@ def austin() -> pd.DataFrame:
     Extracts needed columns from the Austin data and saves it as a csv file
     :return austin_stripped: the stripped data as a pandas dataframe
     """
-    austin_raw = pd.read_csv("./raw_data/austin.csv")
+    AUSTIN_RAW_PATH = "./raw_data/austin.csv"
+    AUSTIN_STRIPED_PATH = "./stripped_data/austin.csv"
+    austin_raw = pd.read_csv(AUSTIN_RAW_PATH)
     austin_stripped = austin_raw[austin_raw.contractor_trade == "Electrical Contractor"]
     austin_stripped = austin_stripped[["issue_date", "location", "contractor_company_name"]]
     latitude = []
@@ -29,7 +31,7 @@ def austin() -> pd.DataFrame:
     austin_stripped = austin_stripped.dropna(subset=["contractor_company_name"])
     austin_stripped.drop(columns=["location"], inplace=True)
     austin_stripped["issue_date"] = austin_stripped["issue_date"].apply(lambda x: x.split("T")[0])
-    austin_stripped.to_csv("./stripped_data/austin.csv")
+    austin_stripped.to_csv(AUSTIN_STRIPED_PATH)
     logging.info("Saved austin.csv")
     return austin_stripped
 
@@ -38,14 +40,16 @@ def new_york() -> pd.DataFrame:
     Extracts needed columns from the New York data and saves it as a csv file
     :return new_york_stripped: the stripped data as a pandas dataframe
     """
-    new_york_raw = pd.read_csv("./raw_data/new_york.csv")
+    NEW_YORK_RAW_PATH = "./raw_data/new_york.csv"
+    NEW_YORK_STRIPPED_PATH = "./stripped_data/new_york.csv"
+    new_york_raw = pd.read_csv(NEW_YORK_RAW_PATH)
     new_york_stripped = new_york_raw[["job_start_date","firm_name", 'gis_latitude', 'gis_longitude']]
     new_york_stripped = new_york_stripped.dropna(subset=["firm_name"])
     if type(new_york_stripped["job_start_date"]) != str:
         new_york_stripped["job_start_date"] = "nan"
     else:
         new_york_stripped["job_start_date"] = new_york_stripped["job_start_date"].apply(lambda x: x.split("T")[0])
-    new_york_stripped.to_csv("./stripped_data/new_york.csv")
+    new_york_stripped.to_csv(NEW_YORK_STRIPPED_PATH)
     logging.info("Saved new_york.csv")
     return new_york_stripped
 
@@ -54,12 +58,14 @@ def chicago() -> pd.DataFrame:
     Extracts needed columns from the Chicago data and saves it as a csv file
     :return: chicago_stripped: the stripped data as a pandas dataframe
     """
-    chicago_raw = pd.read_csv("./raw_data/chicago.csv")
+    CHICAGO_RAW_PATH = "./raw_data/chicago.csv"
+    CHICAGO_STRIPPED_PATH = "./stripped_data/chicago.csv"
+    chicago_raw = pd.read_csv(CHICAGO_RAW_PATH)
     chicago_stripped = chicago_raw[chicago_raw.contact_1_type == "CONTRACTOR-ELECTRICAL"]
     chicago_stripped = chicago_stripped[["issue_date", "contact_1_name", "latitude", "longitude"]]
     chicago_stripped = chicago_stripped.dropna(subset=["contact_1_name"])
     chicago_stripped["issue_date"] = chicago_stripped["issue_date"].apply(lambda x: x.split("T")[0])
-    chicago_stripped.to_csv("./stripped_data/chicago.csv")
+    chicago_stripped.to_csv(CHICAGO_STRIPPED_PATH)
     logging.info("Saved chicago.csv")
     return chicago_stripped
 
@@ -68,7 +74,9 @@ def philly()-> pd.DataFrame:
     Extracts needed columns from the Philadelphia data and saves it as a csv file
     :return: philly_stripped: the stripped data as a pandas dataframe
     """
-    philly_raw = pd.read_csv("./raw_data/philly.csv")
+    PHILLY_RAW_PATH = "./raw_data/philly.csv"
+    PHILLY_STRIPPED_PATH = "./stripped_data/philly.csv"
+    philly_raw = pd.read_csv(PHILLY_RAW_PATH)
     philly_stripped = philly_raw[philly_raw.permitdescription == "ELECTRICAL PERMIT"]
     # philly_stripped["location"] = f"({philly_stripped['lat']}, {philly_stripped['lng']})"
     # philly_stripped = philly_stripped[["permitnumber", "permittype", "permitissuedate", "location", "contractorname"]]
@@ -80,7 +88,7 @@ def philly()-> pd.DataFrame:
     philly_raw2 = pd.read_csv("./raw_data/philly_valuation.csv")
     philly_raw2 = philly_raw2[["lat", "lng", "market_value"]]
     philly_stripped = pd.merge(philly_stripped, philly_raw2, on=["lat", "lng"], how="left")
-    philly_stripped.to_csv("./stripped_data/philly.csv")
+    philly_stripped.to_csv(PHILLY_STRIPPED_PATH)
     logging.info("Saved philly.csv")
     return philly_stripped
 
@@ -90,11 +98,13 @@ def mesa()-> pd.DataFrame:
     Can't filter by contractor trade
     :return: mesa_stripped: the stripped data as a pandas dataframe
     """
-    mesa_raw = pd.read_csv("./raw_data/mesa.csv")
+    MESA_RAW_PATH = "./raw_data/mesa.csv"
+    MESA_STRIPPED_PATH = "./stripped_data/mesa.csv"
+    mesa_raw = pd.read_csv(MESA_RAW_PATH)
     mesa_stripped = mesa_raw[["issued_date", "contractor_name", "latitude", "longitude"]]
     mesa_stripped = mesa_stripped.dropna(subset=["contractor_name"])
     mesa_stripped["issued_date"] = mesa_stripped["issued_date"].apply(lambda x: x.split("T")[0])
-    mesa_stripped.to_csv("./stripped_data/mesa.csv")
+    mesa_stripped.to_csv(MESA_STRIPPED_PATH)
     logging.info("Saved mesa.csv")
     return mesa_stripped
 
@@ -103,7 +113,9 @@ def la()-> pd.DataFrame:
     Extracts needed columns from the Los Angeles data and saves it as a csv file
     :return: la_stripped: the stripped data as a pandas dataframe
     """
-    la_raw = pd.read_csv("./raw_data/la.csv")
+    LA_RAW_PATH = "./raw_data/la.csv"
+    LA_STRIPPED_PATH = "./stripped_data/la.csv"
+    la_raw = pd.read_csv(LA_RAW_PATH)
     la_stripped = la_raw[["issue_date", "contractors_business_name", "location_1", "permit_type"]]
     latitude = []
     longitude = []
@@ -123,7 +135,7 @@ def la()-> pd.DataFrame:
     la_stripped.drop(columns=["location_1", "permit_type"], inplace=True)
     la_stripped = la_stripped.dropna(subset=["contractors_business_name"])
     la_stripped["issue_date"] = la_stripped["issue_date"].apply(lambda x: x.split("T")[0])
-    la_stripped.to_csv("./stripped_data/la.csv")
+    la_stripped.to_csv(LA_STRIPPED_PATH)
     logging.info("Saved la.csv")
     return la_stripped
 
