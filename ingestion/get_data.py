423ffafaf4708d2db4ea0213d5dc2bdff41b9ae9
bharathjyothi <bjyothi2002@gmail.com>
2023-04-18 16:22:34 -0400

    philly ingestion by date

--- ingestion/get_data.py
+++ ingestion/get_data.py
@@ -12,14 +12,14 @@ META_CITY = {"new_york":("socrata", "data.cityofnewyork.us", "dm9a-ab7w", "job_s
             "mesa": ("socrata", "data.mesaaz.gov", "2gkz-7z4f", "issued_date"),
             "la": ("socrata", "data.lacity.org", "nbyu-2ha9", "issue_date"),
             "austin": ("socrata", "data.austintexas.gov", "3syk-w9eu", "issue_date"),
-            "philly": ("non_socrata", "https://phl.carto.com/api/v2/sql?filename=permits&format=csv&skipfields=cartodb_id,the_geom,the_geom_webmercator&q=SELECT%20*,%20ST_Y(the_geom)%20AS%20lat,%20ST_X(the_geom)%20AS%20lng%20FROM%20permits%20WHERE%20permitissuedate%20%3E=%20%272016-01-01%27"),
-            "philly_valuation": ("non_socrata", "https://phl.carto.com/api/v2/sql?q=SELECT * FROM opa_properties_public")}
+            "philly": ("non_socrata", "https://phl.carto.com/api/v2/sql?filename=permits&format=csv&skipfields=cartodb_id,the_geom,the_geom_webmercator&q=SELECT%20*,%20ST_Y(the_geom)%20AS%20lat,%20ST_X(the_geom)%20AS%20lng%20FROM%20permits%20WHERE%20permitissuedate%20%3E=%20%272016-01-01%27", "permitissuedate"),
+            "philly_valuation": ("non_socrata", "https://opendata-downloads.s3.amazonaws.com/opa_properties_public.csv", "assessment_date")}
 # Number of times the script will try to get data from a Socrata API
 SOCRATA_TRIES = 3
 # Max number of rows to call for the Socrata API for testing
 ROW_LIMIT = 1_000_000_000
 
-def get_data(url, location) -> None:
+def get_data(url, location, date_column) -> None:
     """
     Pulls data from a Non-Socrata source and saves it as a csv file
     :param url: the url of the api
@@ -27,8 +27,19 @@ def get_data(url, location) -> None:
     :return: None
     """
     # Use Pandas to read the CSV file from the URL
-    df = pd.read_csv(url)
     # Save the CSV file to the current directory
+    df = pd.read_csv(url)
+
+    if f"{location}.csv" in os.listdir("./raw_data"):
+         # read the csv file already in raw_data
+        df_original = pd.read_csv(f"./raw_data/{location}.csv")
+
+        # sort the df by the permitissuedate column
+        df_original = df_original.sort_values(by=date_column, ascending=False)
+        most_recent_date = df_original[date_column][0]
+        df = df[df[date_column] > most_recent_date]
+        df = df.append(df_original)
+        
     df.to_csv(f"./raw_data/{location}.csv", index=False)
     logging.info(f"Saved {location}.csv")
 
@@ -57,7 +68,7 @@ def get_socrata_data(url, dataset_id, location, date_column) -> None:
 
 
 def main() -> None:
-    locations = ["new_york", "chicago", "mesa", "la", "austin", "philly", "philly_valuation"]
+    locations = ["philly", "philly_valuation"]
     for place in locations:
         meta = META_CITY[place]
         if meta[0] == "socrata":
@@ -73,7 +84,7 @@ def main() -> None:
                     pass
         elif meta[0] == "non_socrata":
             try:
-                get_data(meta[1], place)
+                get_data(meta[1], place, meta[2])
             except Exception as e:
                 logging.error(e)
 
