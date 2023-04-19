3c6b97bd8af6096af55e64702b57f88a557dfc42
wjz224 <wjz224@lehigh.edu>
2023-04-19 17:34:06 -0400

    get_data now uses app tokens to fetch data from client

--- ingestion/get_data.py
+++ ingestion/get_data.py
@@ -5,19 +5,19 @@ import pandas as pd
 from sodapy import Socrata
 import os
 import sys
-
+from dotenv import load_dotenv
 # Dictionary of cities and their corresponding API information
-META_CITY = {"new_york":("socrata", "data.cityofnewyork.us", "dm9a-ab7w", "job_start_date"),
-            "chicago": ("socrata", "data.cityofchicago.org", "ydr8-5enu", "issue_date"),
-            "mesa": ("socrata", "data.mesaaz.gov", "2gkz-7z4f", "issued_date"),
-            "la": ("socrata", "data.lacity.org", "nbyu-2ha9", "issue_date"),
-            "austin": ("socrata", "data.austintexas.gov", "3syk-w9eu", "issue_date"),
-            "philly": ("non_socrata", "https://phl.carto.com/api/v2/sql?filename=permits&format=csv&skipfields=cartodb_id,the_geom,the_geom_webmercator&q=SELECT%20*,%20ST_Y(the_geom)%20AS%20lat,%20ST_X(the_geom)%20AS%20lng%20FROM%20permits%20WHERE%20permitissuedate%20%3E=%20%272016-01-01%27", "permitissuedate"),
-            "philly_valuation": ("non_socrata", "https://opendata-downloads.s3.amazonaws.com/opa_properties_public.csv", "assessment_date")}
+META_CITY = {"new_york":("socrata", "data.cityofnewyork.us", "dm9a-ab7w", "job_start_date", "VMy8JkExlzdzLuhhl0hPPjLOe"),
+            "chicago": ("socrata", "data.cityofchicago.org", "ydr8-5enu", "issue_date", "OeO911BbttwbWOl2CkPqg8MJP"),
+            "mesa": ("socrata", "data.mesaaz.gov", "2gkz-7z4f", "issued_date", "ibJlwU98NuWQ0xQoN4IA9FvNU"),
+            "la": ("socrata", "data.lacity.org", "nbyu-2ha9", "issue_date", "UKh6YfyKpAyOjVFPdmJBTMO6K"),
+            "austin": ("socrata", "data.austintexas.gov", "3syk-w9eu", "issue_date", "BYLqtbK6NfyFXdfpPDrVEA2uO"),
+            "philly": ("non_socrata", "https://phl.carto.com/api/v2/sql?filename=permits&format=csv&skipfields=cartodb_id,the_geom,the_geom_webmercator&q=SELECT%20*,%20ST_Y(the_geom)%20AS%20lat,%20ST_X(the_geom)%20AS%20lng%20FROM%20permits%20WHERE%20permitissuedate%20%3E=%20%272016-01-01%27"),
+            "philly_valuation": ("non_socrata", "https://phl.carto.com/api/v2/sql?q=SELECT * FROM opa_properties_public")}
 # Number of times the script will try to get data from a Socrata API
 SOCRATA_TRIES = 3
 # Max number of rows to call for the Socrata API for testing
-ROW_LIMIT = 1_000#_000_000
+ROW_LIMIT = 1_000
 
 def get_data(url, location, date_column) -> None:
     """
@@ -44,7 +44,7 @@ def get_data(url, location, date_column) -> None:
     logging.info(f"Saved {location}.csv")
 
 
-def get_socrata_data(url, dataset_id, location, date_column) -> None:
+def get_socrata_data(url, dataset_id, location, date_column, app_token) -> None:
     """
     Pulls data from a Socrata API and saves it as a csv file
     :param url: the url of the api
@@ -52,7 +52,10 @@ def get_socrata_data(url, dataset_id, location, date_column) -> None:
     :param location: the name of the location
     :param date_column: the name of the date column
     """
-    client = Socrata(url, None)
+    # load .env file containing socrata account information
+    load_dotenv()
+    # load client 
+    client = Socrata(url, app_token, username = os.getenv("email"), password = os.getenv("password"), timeout = 10)
     # check if the dataset exists in raw_data
     if f"{location}.csv" in os.listdir("./raw_data"):
         df = pd.read_csv(f"./raw_data/{location}.csv")
@@ -79,7 +82,7 @@ def main() -> None:
             fail_count = 0
             while fail_count < SOCRATA_TRIES:
                 try:
-                    get_socrata_data(meta[1], meta[2], place, meta[3])
+                    get_socrata_data(meta[1], meta[2], place, meta[3], meta[4])
                     break
                 except Exception as e:
                     logging.error(e)
